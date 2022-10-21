f59d7728c5a95f04deceb3b9e78966374c340bab
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-21 17:49:28 -0300

    Combobox of colors was added

--- screen/ScreenOne.java
+++ screen/ScreenOne.java
@@ -25,38 +25,50 @@ public class ScreenOne extends JFrame {
     //Notebook
     JLabel notebook = new JLabel("Notebook");
     String[] brandNotebook1 = {"Cícero", "Faber-Castell", "Foroni", "Jandaia", "Spiral", "Tilibra" };
+    String[] colorNotebook1 = {"None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
     JComboBox brandNotebook2 = new JComboBox(brandNotebook1);
+    JComboBox colorNotebook2 = new JComboBox(colorNotebook1);
 
     //Paper
     JLabel paper = new JLabel("Paper");
     String[] brandPaper1 = {"Canson", "Chamex", "Filipaper", "HP", "Jandaia", "Report"};
+    String[] colorPaper1 = {"None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
     JComboBox brandPaper2 = new JComboBox(brandPaper1);
+    JComboBox colorPaper2 = new JComboBox(colorPaper1);
 
     //Pen
     JLabel pen = new JLabel("Pen");
     String[] brandPen1 = {"Bic", "Cis", "Faber-Castell", "Pilot", "Stabilo", "Tombow"};
+    String[] colorPen1 = {"None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
     JComboBox brandPen2 = new JComboBox(brandPen1);
+    JComboBox colorPen2 = new JComboBox(colorPen1);
 
     //Pencil
     JLabel pencil = new JLabel("Pencil");
     String[] brandPencil1 = {"Bic", "Faber-Castell", "Lyra", "Maped", "Molin", "Staedtler"};
+    String[] colorPencil1 = {"None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
     JComboBox brandPencil2 = new JComboBox(brandPencil1);
+    JComboBox colorPencil2 = new JComboBox(colorPencil1);
 
     //Scissors
     JLabel scissors = new JLabel("Scissor");
     String[] brandScissors1 = {"BRW", "Cis", "Maped", "Mundial", "Tilibra", "Tramontina"};
+    String[] colorScissors1 = {"None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
     JComboBox brandScissors2 = new JComboBox(brandScissors1);
+    JComboBox colorScissors2 = new JComboBox(colorScissors1);
 
     //Sharpener
     JLabel sharpener = new JLabel("Sharpener");
     String[] brandSharpener1 = {"Cis", "Faber-Castell", "Lenora", "Maped", "Stabilo", "Tilibra"};
+    String[] colorSharpener1 = {"None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
     JComboBox brandSharpener2 = new JComboBox(brandSharpener1);
+    JComboBox colorSharpener2 = new JComboBox(colorSharpener1);
 
     public ScreenOne(){
 
         //Layout
         setLayout(new FlowLayout());
-        setLayout(new GridLayout(5, 7));
+        setLayout(new GridLayout(12, 15));
         pack();
         setVisible(true);
         //setSize(600,800);
@@ -71,37 +83,38 @@ public class ScreenOne extends JFrame {
         //Glue
         getContentPane().add(glue);
         getContentPane().add(brandGlue2);
-        getContentPane().add();
+        getContentPane().add(colorGlue2);
+
 
         //Notebook
         getContentPane().add(notebook);
         getContentPane().add(brandNotebook2);
-        getContentPane().add();
+        getContentPane().add(colorNotebook2);
 
         //Paper
         getContentPane().add(paper);
         getContentPane().add(brandPaper2);
-        getContentPane().add();
+        getContentPane().add(colorPaper2);
 
         //Pen
         getContentPane().add(pen);
         getContentPane().add(brandPen2);
-        getContentPane().add();
+        getContentPane().add(colorPen2);
 
         //Pencil
         getContentPane().add(pencil);
         getContentPane().add(brandPencil2);
-        getContentPane().add();
+        getContentPane().add(colorPencil2);
 
         //Scissors
         getContentPane().add(scissors);
         getContentPane().add(brandScissors2);
-        getContentPane().add();
+        getContentPane().add(colorScissors2);
 
         //Sharpener
         getContentPane().add(sharpener);
         getContentPane().add(brandSharpener2);
-        getContentPane().add();
+        getContentPane().add(colorSharpener2);
 
         //Buttons
         getContentPane().add(save);
