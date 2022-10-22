2e6d5c5857e429446cfd4c02cdb95c339e76a442
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-22 19:39:21 -0300

    Layout changes (not finalized)

--- screen/ScreenOne.java
+++ screen/ScreenOne.java
@@ -9,8 +9,11 @@ public class ScreenOne extends JFrame {
     JButton delete = new JButton("Delete");
     JButton save = new JButton("Save");
 
+    //Frame
+    JFrame frame = new JFrame("Stock");
+
     //Generic Array
-    String[] colorArray = {"None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
+    String[] colorArray = {"", "None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
 
     //Generic Labels
     JLabel product = new JLabel("Product");
@@ -20,9 +23,12 @@ public class ScreenOne extends JFrame {
     JLabel unitPrice = new JLabel("Unit Price");
     JLabel totalAmount= new JLabel("Total Amount");
 
+    //Panel
+    JPanel panel = new JPanel();
+
     //Eraser
     JLabel eraser = new JLabel("Eraser");
-    String[] brandEraser1 = {"Cis", "Faber-Castell", "Mercur", "Pentel", "Stabilo", "Staedtler"};
+    String[] brandEraser1 = {"","Cis", "Faber-Castell", "Mercur", "Pentel", "Stabilo", "Staedtler"};
     JComboBox brandEraser2 = new JComboBox(brandEraser1);
     JComboBox colorEraser1 = new JComboBox(colorArray);
     JTextField eraserQuantity = new JTextField(null, 1);
@@ -31,7 +37,7 @@ public class ScreenOne extends JFrame {
 
     //Glue
     JLabel glue = new JLabel("Glue");
-    String[] brandGlue1 = {"Acrilex", "Apricola", "Bic", "Faber-Castell", "Pritt", "Tenaz"};
+    String[] brandGlue1 = {"", "Acrilex", "Apricola", "Bic", "Faber-Castell", "Pritt", "Tenaz"};
     JComboBox brandGlue2 = new JComboBox(brandGlue1);
     JComboBox colorGlue1 = new JComboBox(colorArray);
     JTextField glueQuantity = new JTextField(null, 1);
@@ -40,7 +46,7 @@ public class ScreenOne extends JFrame {
 
     //Notebook
     JLabel notebook = new JLabel("Notebook");
-    String[] brandNotebook1 = {"Cícero", "Faber-Castell", "Foroni", "Jandaia", "Spiral", "Tilibra" };
+    String[] brandNotebook1 = {"", "Cícero", "Faber-Castell", "Foroni", "Jandaia", "Spiral", "Tilibra" };
     JComboBox brandNotebook2 = new JComboBox(brandNotebook1);
     JComboBox colorNotebook1 = new JComboBox(colorArray);
     JTextField notebookQuantity = new JTextField(null, 1);
@@ -49,7 +55,7 @@ public class ScreenOne extends JFrame {
 
     //Paper
     JLabel paper = new JLabel("Paper");
-    String[] brandPaper1 = {"Canson", "Chamex", "Filipaper", "HP", "Jandaia", "Report"};
+    String[] brandPaper1 = {"", "Canson", "Chamex", "Filipaper", "HP", "Jandaia", "Report"};
     JComboBox brandPaper2 = new JComboBox(brandPaper1);
     JComboBox colorPaper1 = new JComboBox(colorArray);
     JTextField paperQuantity = new JTextField(null, 1);
@@ -58,7 +64,7 @@ public class ScreenOne extends JFrame {
 
     //Pen
     JLabel pen = new JLabel("Pen");
-    String[] brandPen1 = {"Bic", "Cis", "Faber-Castell", "Pilot", "Stabilo", "Tombow"};
+    String[] brandPen1 = {"", "Bic", "Cis", "Faber-Castell", "Pilot", "Stabilo", "Tombow"};
     JComboBox brandPen2 = new JComboBox(brandPen1);
     JComboBox colorPen1 = new JComboBox(colorArray);
     JTextField penQuantity = new JTextField(null, 1);
@@ -67,7 +73,7 @@ public class ScreenOne extends JFrame {
 
     //Pencil
     JLabel pencil = new JLabel("Pencil");
-    String[] brandPencil1 = {"Bic", "Faber-Castell", "Lyra", "Maped", "Molin", "Staedtler"};
+    String[] brandPencil1 = {"", "Bic", "Faber-Castell", "Lyra", "Maped", "Molin", "Staedtler"};
     JComboBox brandPencil2 = new JComboBox(brandPencil1);
     JComboBox colorPencil1 = new JComboBox(colorArray);
     JTextField pencilQuantity = new JTextField(null, 1);
@@ -76,7 +82,7 @@ public class ScreenOne extends JFrame {
 
     //Scissors
     JLabel scissors = new JLabel("Scissor");
-    String[] brandScissors1 = {"BRW", "Cis", "Maped", "Mundial", "Tilibra", "Tramontina"};
+    String[] brandScissors1 = {"", "BRW", "Cis", "Maped", "Mundial", "Tilibra", "Tramontina"};
     JComboBox brandScissors2 = new JComboBox(brandScissors1);
     JComboBox colorScissors1 = new JComboBox(colorArray);
     JTextField scissorsQuantity = new JTextField(null, 1);
@@ -85,136 +91,209 @@ public class ScreenOne extends JFrame {
 
     //Sharpener
     JLabel sharpener = new JLabel("Sharpener");
-    String[] brandSharpener1 = {"Cis", "Faber-Castell", "Lenora", "Maped", "Stabilo", "Tilibra"};
+    String[] brandSharpener1 = {"", "Cis", "Faber-Castell", "Lenora", "Maped", "Stabilo", "Tilibra"};
     JComboBox brandSharpener2 = new JComboBox(brandSharpener1);
     JComboBox colorSharpener1 = new JComboBox(colorArray);
     JTextField sharpenerQuantity = new JTextField(null, 1);
     JTextField sharpenerUnitPrice = new JTextField(null, 1);
     JTextField sharpenerTotalAmount = new JTextField(null, 1);
 
-    public ScreenOne(){
+    public ScreenOne() {
+
+        //Frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
 
         //Layout
-        setLayout(new FlowLayout());
-        setLayout(new GridLayout(10, 8));
-        setBackground(new Color(0xffffff));
-        //pack(setMinimumSize());
-        setVisible(true);
-        setSize(800,600);
+        panel.setSize(800, 600);
+        //panel.setLayout(new FlowLayout());
+        panel.setBackground(Color.cyan);
+
 
         //Generic Labels
-        getContentPane().add(product);
+        panel.add(product);
+        product.setPreferredSize(new Dimension(100, 50));
         product.setFont(new Font("Arial", Font.BOLD, 16));
         product.setForeground(new Color(0x000000));
-        getContentPane().add(brand);
+        product.setBounds(100, 20, 100, 50);
+        panel.add(brand);
+        brand.setPreferredSize(new Dimension(100, 50));
         brand.setFont(new Font("Arial", Font.BOLD, 16));
         brand.setForeground(new Color(0x000000));
-        getContentPane().add(color);
+        brand.setBounds(200, 20, 100, 50);
+        panel.add(color);
+        color.setPreferredSize(new Dimension(100, 50));
         color.setFont(new Font("Arial", Font.BOLD, 16));
         color.setForeground(new Color(0x000000));
-        getContentPane().add(quantity);
+        color.setBounds(300, 20, 100, 50);
+        panel.add(quantity);
+        quantity.setPreferredSize(new Dimension(100, 50));
         quantity.setFont(new Font("Arial", Font.BOLD, 16));
         quantity.setForeground(new Color(0x000000));
-        getContentPane().add(unitPrice);
+        quantity.setBounds(400, 20, 100, 50);
+        panel.add(unitPrice);
+        unitPrice.setPreferredSize(new Dimension(100, 50));
         unitPrice.setFont(new Font("Arial", Font.BOLD, 16));
         unitPrice.setForeground(new Color(0x000000));
-        getContentPane().add(totalAmount);
+        unitPrice.setBounds(500, 20, 100, 50);
+        panel.add(totalAmount);
+        totalAmount.setPreferredSize(new Dimension(100, 50));
         totalAmount.setFont(new Font("Arial", Font.BOLD, 16));
         totalAmount.setForeground(new Color(0x000000));
+        totalAmount.setBounds(600, 20, 100, 50);
 
         //Eraser
         eraser.setFont(new Font("Arial", Font.PLAIN, 16));
         eraser.setForeground(new Color(0x000000));
-        getContentPane().add(eraser);
-        getContentPane().add(brandEraser2);
-        getContentPane().add(colorEraser1);
-        getContentPane().add(eraserQuantity);
-        getContentPane().add(eraserUnitPrice);
-        getContentPane().add(eraserTotalAmount);
+        panel.add(eraser);
+        eraser.setBounds(100, 100, 100, 50);
+        panel.add(brandEraser2);
+        brandEraser2.setBounds(200, 100, 100, 50);
+        panel.add(colorEraser1);
+        colorEraser1.setBounds(300, 100, 100, 50);
+        panel.add(eraserQuantity);
+        eraserQuantity.setBounds(400, 100, 100, 50);
+        panel.add(eraserUnitPrice);
+        eraserUnitPrice.setBounds(500, 100, 100, 50);
+        panel.add(eraserTotalAmount);
+        eraserTotalAmount.setBounds(600, 100, 100, 50);
 
         //Glue
         glue.setFont(new Font("Arial", Font.PLAIN, 16));
         glue.setForeground(new Color(0x000000));
-        getContentPane().add(glue);
-        getContentPane().add(brandGlue2);
-        getContentPane().add(colorGlue1);
-        getContentPane().add(glueQuantity);
-        getContentPane().add(glueUnitPrice);
-        getContentPane().add(glueTotalAmount);
+        panel.add(glue);
+        glue.setBounds(100, 150, 100, 50);
+        panel.add(brandGlue2);
+        brandGlue2.setBounds(200, 150, 100, 50);
+        panel.add(colorGlue1);
+        colorGlue1.setBounds(300, 150, 100, 50);
+        panel.add(glueQuantity);
+        glueQuantity.setBounds(400, 150, 100, 50);
+        panel.add(glueUnitPrice);
+        glueUnitPrice.setBounds(500, 150, 100, 50);
+        panel.add(glueTotalAmount);
+        glueTotalAmount.setBounds(600, 150, 100, 50);
 
         //Notebook
         notebook.setFont(new Font("Arial", Font.PLAIN, 16));
         notebook.setForeground(new Color(0x000000));
-        getContentPane().add(notebook);
-        getContentPane().add(brandNotebook2);
-        getContentPane().add(colorNotebook1);
-        getContentPane().add(notebookQuantity);
-        getContentPane().add(notebookUnitPrice);
-        getContentPane().add(notebookTotalAmount);
+        panel.add(notebook);
+        notebook.setBounds(100, 200, 100, 50);
+        panel.add(brandNotebook2);
+        brandNotebook2.setBounds(200, 200, 100, 50);
+        panel.add(colorNotebook1);
+        colorNotebook1.setBounds(300, 200, 100, 50);
+        panel.add(notebookQuantity);
+        notebookQuantity.setBounds(400, 200, 100, 50);
+        panel.add(notebookUnitPrice);
+        notebookUnitPrice.setBounds(500, 200, 100, 50);
+        panel.add(notebookTotalAmount);
+        notebookTotalAmount.setBounds(600, 200, 100, 50);
 
         //Paper
         paper.setFont(new Font("Arial", Font.PLAIN, 16));
         paper.setForeground(new Color(0x000000));
-        getContentPane().add(paper);
-        getContentPane().add(brandPaper2);
-        getContentPane().add(colorPaper1);
-        getContentPane().add(paperQuantity);
-        getContentPane().add(paperUnitPrice);
-        getContentPane().add(paperTotalAmount);
+        panel.add(paper);
+        paper.setBounds(100, 250, 100, 50);
+        panel.add(brandPaper2);
+        brandPaper2.setBounds(200, 250, 100, 50);
+        panel.add(colorPaper1);
+        colorPaper1.setBounds(300, 250, 100, 50);
+        panel.add(paperQuantity);
+        paperQuantity.setBounds(400, 250, 100, 50);
+        panel.add(paperUnitPrice);
+        paperUnitPrice.setBounds(500, 250, 100, 50);
+        panel.add(paperTotalAmount);
+        paperTotalAmount.setBounds(600, 250, 100, 50);
 
         //Pen
         pen.setFont(new Font("Arial", Font.PLAIN, 16));
         pen.setForeground(new Color(0x000000));
-        getContentPane().add(pen);
-        getContentPane().add(brandPen2);
-        getContentPane().add(colorPen1);
-        getContentPane().add(penQuantity);
-        getContentPane().add(penUnitPrice);
-        getContentPane().add(penTotalAmount);
+        panel.add(pen);
+        pen.setBounds(100, 300, 100, 50);
+        panel.add(brandPen2);
+        brandPen2.setBounds(200, 300, 100, 50);
+        panel.add(colorPen1);
+        colorPen1.setBounds(300, 300, 100, 50);
+        panel.add(penQuantity);
+        penQuantity.setBounds(400, 300, 100, 50);
+        panel.add(penUnitPrice);
+        penUnitPrice.setBounds(500, 300, 100, 50);
+        panel.add(penTotalAmount);
+        penTotalAmount.setBounds(600, 300, 100, 50);
 
         //Pencil
         pencil.setFont(new Font("Arial", Font.PLAIN, 16));
         pencil.setForeground(new Color(0x000000));
-        getContentPane().add(pencil);
-        getContentPane().add(brandPencil2);
-        getContentPane().add(colorPencil1);
-        getContentPane().add(pencilQuantity);
-        getContentPane().add(pencilUnitPrice);
-        getContentPane().add(pencilTotalAmount);
+        panel.add(pencil);
+        pencil.setBounds(100, 350, 100, 50);
+        panel.add(brandPencil2);
+        brandPencil2.setBounds(200, 350, 100, 50);
+        panel.add(colorPencil1);
+        colorPencil1.setBounds(300, 350, 100, 50);
+        panel.add(pencilQuantity);
+        pencilQuantity.setBounds(400, 350, 100, 50);
+        panel.add(pencilUnitPrice);
+        pencilUnitPrice.setBounds(500, 350, 100, 50);
+        panel.add(pencilTotalAmount);
+        pencilTotalAmount.setBounds(600, 350, 100, 50);
 
         //Scissors
         scissors.setFont(new Font("Arial", Font.PLAIN, 16));
         scissors.setForeground(new Color(0x000000));
-        getContentPane().add(scissors);
-        getContentPane().add(brandScissors2);
-        getContentPane().add(colorScissors1);
-        getContentPane().add(scissorsQuantity);
-        getContentPane().add(scissorsUnitPrice);
-        getContentPane().add(scissorsTotalAmount);
+        panel.add(scissors);
+        scissors.setBounds(100, 400, 100, 50);
+        panel.add(brandScissors2);
+        brandScissors2.setBounds(200, 400, 100, 50);
+        panel.add(colorScissors1);
+        colorScissors1.setBounds(300, 400, 100, 50);
+        panel.add(scissorsQuantity);
+        scissorsQuantity.setBounds(400, 400, 100, 50);
+        panel.add(scissorsUnitPrice);
+        scissorsUnitPrice.setBounds(500, 400, 100, 50);
+        panel.add(scissorsTotalAmount);
+        scissorsTotalAmount.setBounds(600, 400, 100, 50);
 
         //Sharpener
         sharpener.setFont(new Font("Arial", Font.PLAIN, 16));
         sharpener.setForeground(new Color(0x000000));
-        getContentPane().add(sharpener);
-        getContentPane().add(brandSharpener2);
-        getContentPane().add(colorSharpener1);
-        getContentPane().add(sharpenerQuantity);
-        getContentPane().add(sharpenerUnitPrice);
-        getContentPane().add(sharpenerTotalAmount);
+        panel.add(sharpener);
+        sharpener.setBounds(100, 450, 100, 50);
+        panel.add(brandSharpener2);
+        brandSharpener2.setBounds(200, 450, 100, 50);
+        panel.add(colorSharpener1);
+        colorSharpener1.setBounds(300, 450, 100, 50);
+        panel.add(sharpenerQuantity);
+        sharpenerQuantity.setBounds(400, 450, 100, 50);
+        panel.add(sharpenerUnitPrice);
+        sharpenerUnitPrice.setBounds(500, 450, 100, 50);
+        panel.add(sharpenerTotalAmount);
+        sharpenerTotalAmount.setBounds(600, 450, 100, 50);
 
         //Button Save
+        panel.add(save);
+        //save.setBounds(300, 520, 100, 50);
+        //save.setSize(100, 50);
+        //save.setLayout(null);
+        //save.setVisible(true);
         save.setFont(new Font("Arial", Font.PLAIN, 16));
         save.setForeground(new Color(0x000000));
         save.setBackground(new Color(0xffffff));
         save.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED,Color.LIGHT_GRAY, Color.darkGray));
-        getContentPane().add(save);
+        //save.setHorizontalAlignment(SwingConstants.CENTER);
 
         //Button Delete
+        panel.add(delete);
+        //delete.setLayout(null);
+        //delete.setVisible(true);
+        //delete.setBounds(500, 520, 100, 50);
         delete.setFont(new Font("Arial", Font.PLAIN, 16));
         delete.setForeground(new Color(0x000000));
         delete.setBackground(new Color(0xffffff));
         delete.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED,Color.LIGHT_GRAY, Color.darkGray));
-        getContentPane().add(delete);
+        //delete.setHorizontalAlignment(SwingConstants.CENTER);
 
     }
 
