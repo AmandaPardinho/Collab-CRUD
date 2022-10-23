4504ae6d79e5c5c908e6c56508828e697b1b3ca2
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-23 02:05:20 -0300

    Layout and Listener
    -Changes in Layout;
    -Started to create an event by clicking the button.

---
+++ screen/ActionButton.java
@@ -0,0 +1,30 @@
+/*package screen;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ActionButton implements ActionListener {
+
+    private actionSave;
+    private actionDelete;
+    public ActionButton(){
+        this.actionDelete = actionDelete;
+    }
+
+    public ActionButton(){
+        this.actionSave = actionSave;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //String remove = actionDelete.getText();
+        //String saving = actionSave.getText();
+        if(saveAction.getOnMouseClick){
+            JOptionPane.showMessageDialog(null, "Object was saved successfully");
+        }else{
+            JOptionPane.showMessageDialog(null, "Object was deleted successfully");
+        }
+
+
+    }
+}*/
--- screen/ScreenOne.java
+++ screen/ScreenOne.java
@@ -1,7 +1,6 @@
 package screen;
 
 import javax.swing.*;
-import javax.swing.border.BevelBorder;
 import java.awt.*;
 
 public class ScreenOne extends JFrame {
@@ -31,114 +30,128 @@ public class ScreenOne extends JFrame {
     String[] brandEraser1 = {"","Cis", "Faber-Castell", "Mercur", "Pentel", "Stabilo", "Staedtler"};
     JComboBox brandEraser2 = new JComboBox(brandEraser1);
     JComboBox colorEraser1 = new JComboBox(colorArray);
-    JTextField eraserQuantity = new JTextField(null, 1);
-    JTextField eraserUnitPrice = new JTextField(null, 1);
-    JTextField eraserTotalAmount = new JTextField(null, 1);
+    JTextField eraserQuantity = new JTextField(null, 8);
+    JTextField eraserUnitPrice = new JTextField(null, 8);
+    JTextField eraserTotalAmount = new JTextField(null, 8);
 
     //Glue
     JLabel glue = new JLabel("Glue");
     String[] brandGlue1 = {"", "Acrilex", "Apricola", "Bic", "Faber-Castell", "Pritt", "Tenaz"};
     JComboBox brandGlue2 = new JComboBox(brandGlue1);
     JComboBox colorGlue1 = new JComboBox(colorArray);
-    JTextField glueQuantity = new JTextField(null, 1);
-    JTextField glueUnitPrice= new JTextField(null, 1);
-    JTextField glueTotalAmount= new JTextField(null, 1);
+    JTextField glueQuantity = new JTextField(null, 8);
+    JTextField glueUnitPrice= new JTextField(null, 8);
+    JTextField glueTotalAmount= new JTextField(null, 8);
 
     //Notebook
     JLabel notebook = new JLabel("Notebook");
     String[] brandNotebook1 = {"", "Cícero", "Faber-Castell", "Foroni", "Jandaia", "Spiral", "Tilibra" };
     JComboBox brandNotebook2 = new JComboBox(brandNotebook1);
     JComboBox colorNotebook1 = new JComboBox(colorArray);
-    JTextField notebookQuantity = new JTextField(null, 1);
-    JTextField notebookUnitPrice = new JTextField(null, 1);
-    JTextField notebookTotalAmount = new JTextField(null, 1);
+    JTextField notebookQuantity = new JTextField(null, 6);
+    JTextField notebookUnitPrice = new JTextField(null, 6);
+    JTextField notebookTotalAmount = new JTextField(null, 6);
 
     //Paper
     JLabel paper = new JLabel("Paper");
     String[] brandPaper1 = {"", "Canson", "Chamex", "Filipaper", "HP", "Jandaia", "Report"};
     JComboBox brandPaper2 = new JComboBox(brandPaper1);
     JComboBox colorPaper1 = new JComboBox(colorArray);
-    JTextField paperQuantity = new JTextField(null, 1);
-    JTextField paperUnitPrice = new JTextField(null, 1);
-    JTextField paperTotalAmount = new JTextField(null, 1);
+    JTextField paperQuantity = new JTextField(null, 6);
+    JTextField paperUnitPrice = new JTextField(null, 6);
+    JTextField paperTotalAmount = new JTextField(null, 6);
 
     //Pen
     JLabel pen = new JLabel("Pen");
     String[] brandPen1 = {"", "Bic", "Cis", "Faber-Castell", "Pilot", "Stabilo", "Tombow"};
     JComboBox brandPen2 = new JComboBox(brandPen1);
     JComboBox colorPen1 = new JComboBox(colorArray);
-    JTextField penQuantity = new JTextField(null, 1);
-    JTextField penUnitPrice = new JTextField(null, 1);
-    JTextField penTotalAmount = new JTextField(null, 1);
+    JTextField penQuantity = new JTextField(null, 6);
+    JTextField penUnitPrice = new JTextField(null, 6);
+    JTextField penTotalAmount = new JTextField(null, 6);
 
     //Pencil
     JLabel pencil = new JLabel("Pencil");
     String[] brandPencil1 = {"", "Bic", "Faber-Castell", "Lyra", "Maped", "Molin", "Staedtler"};
     JComboBox brandPencil2 = new JComboBox(brandPencil1);
     JComboBox colorPencil1 = new JComboBox(colorArray);
-    JTextField pencilQuantity = new JTextField(null, 1);
-    JTextField pencilUnitPrice = new JTextField(null, 1);
-    JTextField pencilTotalAmount = new JTextField(null, 1);
+    JTextField pencilQuantity = new JTextField(null, 6);
+    JTextField pencilUnitPrice = new JTextField(null, 6);
+    JTextField pencilTotalAmount = new JTextField(null, 6);
 
     //Scissors
     JLabel scissors = new JLabel("Scissor");
     String[] brandScissors1 = {"", "BRW", "Cis", "Maped", "Mundial", "Tilibra", "Tramontina"};
     JComboBox brandScissors2 = new JComboBox(brandScissors1);
     JComboBox colorScissors1 = new JComboBox(colorArray);
-    JTextField scissorsQuantity = new JTextField(null, 1);
-    JTextField scissorsUnitPrice = new JTextField(null, 1);
-    JTextField scissorsTotalAmount = new JTextField(null, 1);
+    JTextField scissorsQuantity = new JTextField(null, 6);
+    JTextField scissorsUnitPrice = new JTextField(null, 6);
+    JTextField scissorsTotalAmount = new JTextField(null, 6);
 
     //Sharpener
     JLabel sharpener = new JLabel("Sharpener");
     String[] brandSharpener1 = {"", "Cis", "Faber-Castell", "Lenora", "Maped", "Stabilo", "Tilibra"};
     JComboBox brandSharpener2 = new JComboBox(brandSharpener1);
     JComboBox colorSharpener1 = new JComboBox(colorArray);
-    JTextField sharpenerQuantity = new JTextField(null, 1);
-    JTextField sharpenerUnitPrice = new JTextField(null, 1);
-    JTextField sharpenerTotalAmount = new JTextField(null, 1);
+    JTextField sharpenerQuantity = new JTextField(null, 6);
+    JTextField sharpenerUnitPrice = new JTextField(null, 6);
+    JTextField sharpenerTotalAmount = new JTextField(null, 6);
 
     public ScreenOne() {
 
         //Frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         frame.setSize(800, 600);
+        frame.setLocation(0,0);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //programa termina ao fechar a janela
         frame.setVisible(true);
 
         //Layout
         panel.setSize(800, 600);
-        //panel.setLayout(new FlowLayout());
+        panel.setLocation(0,0);
         panel.setBackground(Color.cyan);
-
+        panel.setLayout(new GridLayout(10, 6));
+        panel.setVisible(true);
 
         //Generic Labels
         panel.add(product);
+        product.setBounds(100, 20, 100, 50);
+        //product.setAlignmentX(100);
+        //product.setAlignmentY(20);
         product.setPreferredSize(new Dimension(100, 50));
         product.setFont(new Font("Arial", Font.BOLD, 16));
         product.setForeground(new Color(0x000000));
-        product.setBounds(100, 20, 100, 50);
         panel.add(brand);
+        //brand.setAlignmentX(200);
+        //brand.setAlignmentY(20);
         brand.setPreferredSize(new Dimension(100, 50));
         brand.setFont(new Font("Arial", Font.BOLD, 16));
         brand.setForeground(new Color(0x000000));
         brand.setBounds(200, 20, 100, 50);
         panel.add(color);
+        //color.setAlignmentX(300);
+        //color.setAlignmentY(20);
         color.setPreferredSize(new Dimension(100, 50));
         color.setFont(new Font("Arial", Font.BOLD, 16));
         color.setForeground(new Color(0x000000));
         color.setBounds(300, 20, 100, 50);
         panel.add(quantity);
+        //quantity.setAlignmentX(400);
+        //quantity.setAlignmentY(20);
         quantity.setPreferredSize(new Dimension(100, 50));
         quantity.setFont(new Font("Arial", Font.BOLD, 16));
         quantity.setForeground(new Color(0x000000));
         quantity.setBounds(400, 20, 100, 50);
         panel.add(unitPrice);
+        //unitPrice.setAlignmentX(500);
+        //unitPrice.setAlignmentY(20);
         unitPrice.setPreferredSize(new Dimension(100, 50));
         unitPrice.setFont(new Font("Arial", Font.BOLD, 16));
         unitPrice.setForeground(new Color(0x000000));
         unitPrice.setBounds(500, 20, 100, 50);
         panel.add(totalAmount);
+        //totalAmount.setAlignmentX(600);
+        //totalAmount.setAlignmentY(20);
         totalAmount.setPreferredSize(new Dimension(100, 50));
         totalAmount.setFont(new Font("Arial", Font.BOLD, 16));
         totalAmount.setForeground(new Color(0x000000));
@@ -148,21 +161,33 @@ public class ScreenOne extends JFrame {
         eraser.setFont(new Font("Arial", Font.PLAIN, 16));
         eraser.setForeground(new Color(0x000000));
         panel.add(eraser);
+        eraser.setAlignmentX(100);
+        eraser.setAlignmentY(120);
         eraser.setPreferredSize(new Dimension(100, 50));
         eraser.setBounds(100, 100, 100, 50);
         panel.add(brandEraser2);
+        brandEraser2.setAlignmentX(200);
+        brandEraser2.setAlignmentY(120);
         brandEraser2.setPreferredSize(new Dimension(100, 50));
         brandEraser2.setBounds(200, 100, 100, 50);
         panel.add(colorEraser1);
+        colorEraser1.setAlignmentX(300);
+        colorEraser1.setAlignmentY(120);
         colorEraser1.setPreferredSize(new Dimension(100, 50));
         colorEraser1.setBounds(300, 100, 100, 50);
         panel.add(eraserQuantity);
+        eraserQuantity.setAlignmentX(400);
+        eraserQuantity.setAlignmentY(120);
         eraserQuantity.setPreferredSize(new Dimension(100, 50));
         eraserQuantity.setBounds(400, 100, 100, 50);
         panel.add(eraserUnitPrice);
+        eraserUnitPrice.setAlignmentX(500);
+        eraserUnitPrice.setAlignmentY(120);
         eraserUnitPrice.setPreferredSize(new Dimension(100, 50));
         eraserUnitPrice.setBounds(500, 100, 100, 50);
         panel.add(eraserTotalAmount);
+        eraserTotalAmount.setAlignmentX(600);
+        eraserTotalAmount.setAlignmentY(120);
         eraserTotalAmount.setPreferredSize(new Dimension(100, 50));
         eraserTotalAmount.setBounds(600, 100, 100, 50);
 
@@ -170,21 +195,33 @@ public class ScreenOne extends JFrame {
         glue.setFont(new Font("Arial", Font.PLAIN, 16));
         glue.setForeground(new Color(0x000000));
         panel.add(glue);
+        glue.setAlignmentX(100);
+        glue.setAlignmentY(220);
         glue.setPreferredSize(new Dimension(100, 50));
         glue.setBounds(100, 150, 100, 50);
         panel.add(brandGlue2);
+        brandGlue2.setAlignmentX(200);
+        brandGlue2.setAlignmentY(220);
         brandGlue2.setPreferredSize(new Dimension(100, 50));
         brandGlue2.setBounds(200, 150, 100, 50);
         panel.add(colorGlue1);
+        colorGlue1.setAlignmentX(300);
+        colorGlue1.setAlignmentY(220);
         colorGlue1.setPreferredSize(new Dimension(100, 50));
         colorGlue1.setBounds(300, 150, 100, 50);
         panel.add(glueQuantity);
+        glueQuantity.setAlignmentX(400);
+        glueQuantity.setAlignmentY(220);
         glueQuantity.setPreferredSize(new Dimension(100, 50));
         glueQuantity.setBounds(400, 150, 100, 50);
         panel.add(glueUnitPrice);
+        glueUnitPrice.setAlignmentX(500);
+        glueUnitPrice.setAlignmentY(220);
         glueUnitPrice.setPreferredSize(new Dimension(100, 50));
         glueUnitPrice.setBounds(500, 150, 100, 50);
         panel.add(glueTotalAmount);
+        glueTotalAmount.setAlignmentX(600);
+        glueTotalAmount.setAlignmentY(220);
         glueTotalAmount.setPreferredSize(new Dimension(100, 50));
         glueTotalAmount.setBounds(600, 150, 100, 50);
 
@@ -192,38 +229,67 @@ public class ScreenOne extends JFrame {
         notebook.setFont(new Font("Arial", Font.PLAIN, 16));
         notebook.setForeground(new Color(0x000000));
         panel.add(notebook);
+        notebook.setAlignmentX(100);
+        notebook.setAlignmentY(320);
         notebook.setPreferredSize(new Dimension(100, 50));
         notebook.setBounds(100, 200, 100, 50);
         panel.add(brandNotebook2);
+        brandNotebook2.setAlignmentX(200);
+        brandNotebook2.setAlignmentY(320);
+        brandNotebook2.setPreferredSize(new Dimension(100, 50));
         brandNotebook2.setBounds(200, 200, 100, 50);
         panel.add(colorNotebook1);
+        colorNotebook1.setAlignmentX(300);
+        colorNotebook1.setAlignmentY(320);
+        colorNotebook1.setPreferredSize(new Dimension(100, 50));
         colorNotebook1.setBounds(300, 200, 100, 50);
         panel.add(notebookQuantity);
+        notebookQuantity.setAlignmentX(400);
+        notebookQuantity.setAlignmentY(320);
+        notebookQuantity.setPreferredSize(new Dimension(100, 50));
         notebookQuantity.setBounds(400, 200, 100, 50);
         panel.add(notebookUnitPrice);
+        notebookUnitPrice.setAlignmentX(500);
+        notebookUnitPrice.setAlignmentY(320);
+        notebookUnitPrice.setPreferredSize(new Dimension(100, 50));
         notebookUnitPrice.setBounds(500, 200, 100, 50);
         panel.add(notebookTotalAmount);
+        notebookTotalAmount.setAlignmentX(600);
+        notebookTotalAmount.setAlignmentY(320);
+        notebookTotalAmount.setPreferredSize(new Dimension(100, 50));
         notebookTotalAmount.setBounds(600, 200, 100, 50);
 
         //Paper
         paper.setFont(new Font("Arial", Font.PLAIN, 16));
         paper.setForeground(new Color(0x000000));
         panel.add(paper);
+        notebook.setAlignmentX(100);
+        notebook.setAlignmentY(420);
         paper.setPreferredSize(new Dimension(100, 50));
         paper.setBounds(100, 250, 100, 50);
         panel.add(brandPaper2);
+        brandPaper2.setAlignmentX(200);
+        brandPaper2.setAlignmentY(420);
         brandPaper2.setPreferredSize(new Dimension(100, 50));
         brandPaper2.setBounds(200, 250, 100, 50);
         panel.add(colorPaper1);
+        colorPaper1.setAlignmentX(300);
+        colorPaper1.setAlignmentY(420);
         colorPaper1.setPreferredSize(new Dimension(100, 50));
         colorPaper1.setBounds(300, 250, 100, 50);
         panel.add(paperQuantity);
+        paperQuantity.setAlignmentX(400);
+        paperQuantity.setAlignmentY(420);
         paperQuantity.setPreferredSize(new Dimension(100, 50));
         paperQuantity.setBounds(400, 250, 100, 50);
         panel.add(paperUnitPrice);
+        paperUnitPrice.setAlignmentX(500);
+        paperUnitPrice.setAlignmentY(420);
         paperUnitPrice.setPreferredSize(new Dimension(100, 50));
         paperUnitPrice.setBounds(500, 250, 100, 50);
         panel.add(paperTotalAmount);
+        paperTotalAmount.setAlignmentX(600);
+        paperTotalAmount.setAlignmentY(420);
         paperTotalAmount.setPreferredSize(new Dimension(100, 50));
         paperTotalAmount.setBounds(600, 250, 100, 50);
 
@@ -317,27 +383,35 @@ public class ScreenOne extends JFrame {
 
         //Button Save
         panel.add(save);
-        save.setBounds(300, 520, 100, 50);
-        save.setPreferredSize(new Dimension(100, 50));
-        //save.setLayout(null);
-        //save.setVisible(true);
+        //ActionButton saveAction = new ActionButton();
+        //save.addActionListener(saveAction);
+        save.setBounds(300, 580, 100, 50);
+        //save.setAlignmentX(500);
+        //save.setAlignmentY(580);
+        //save.setHorizontalAlignment(300);
+        //save.setVerticalAlignment(580);
+        //save.setPreferredSize(new Dimension(100, 50));
         save.setFont(new Font("Arial", Font.PLAIN, 16));
         save.setForeground(new Color(0x000000));
         save.setBackground(new Color(0xffffff));
-        save.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED,Color.LIGHT_GRAY, Color.darkGray));
-        //save.setHorizontalAlignment(SwingConstants.CENTER);
+        //save.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED,Color.LIGHT_GRAY, Color.darkGray));
+        //save.setAlignmentX(Component.BOTTOM_ALIGNMENT);
 
         //Button Delete
         panel.add(delete);
-        //delete.setLayout(null);
-        delete.setPreferredSize(new Dimension(100, 50));
-        delete.setBounds(500, 520, 100, 50);
+        //ActionButton deleteAction = new ActionButton();
+        //delete.addActionListener(deleteAction);
+        delete.setBounds(500, 580, 100, 50);
+        delete.setAlignmentX(500);
+        delete.setAlignmentY(580);
+        delete.setHorizontalAlignment(500);
+        delete.setVerticalAlignment(580);
+        //delete.setPreferredSize(new Dimension(100, 50));
         delete.setFont(new Font("Arial", Font.PLAIN, 16));
         delete.setForeground(new Color(0x000000));
         delete.setBackground(new Color(0xffffff));
-        delete.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED,Color.LIGHT_GRAY, Color.darkGray));
-        //delete.setHorizontalAlignment(SwingConstants.CENTER);
-
+        //delete.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED,Color.LIGHT_GRAY, Color.darkGray));
+        delete.setAlignmentX(Component.BOTTOM_ALIGNMENT);
     }
 
     public static void main(String[] args) {
