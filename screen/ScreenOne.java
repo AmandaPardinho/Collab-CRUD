fa1ae7bc0db1765a2d8a93832d325a6d50b76d68
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-22 21:17:32 -0300

    Defining the size of items

--- screen/ScreenOne.java
+++ screen/ScreenOne.java
@@ -148,38 +148,51 @@ public class ScreenOne extends JFrame {
         eraser.setFont(new Font("Arial", Font.PLAIN, 16));
         eraser.setForeground(new Color(0x000000));
         panel.add(eraser);
+        eraser.setPreferredSize(new Dimension(100, 50));
         eraser.setBounds(100, 100, 100, 50);
         panel.add(brandEraser2);
+        brandEraser2.setPreferredSize(new Dimension(100, 50));
         brandEraser2.setBounds(200, 100, 100, 50);
         panel.add(colorEraser1);
+        colorEraser1.setPreferredSize(new Dimension(100, 50));
         colorEraser1.setBounds(300, 100, 100, 50);
         panel.add(eraserQuantity);
+        eraserQuantity.setPreferredSize(new Dimension(100, 50));
         eraserQuantity.setBounds(400, 100, 100, 50);
         panel.add(eraserUnitPrice);
+        eraserUnitPrice.setPreferredSize(new Dimension(100, 50));
         eraserUnitPrice.setBounds(500, 100, 100, 50);
         panel.add(eraserTotalAmount);
+        eraserTotalAmount.setPreferredSize(new Dimension(100, 50));
         eraserTotalAmount.setBounds(600, 100, 100, 50);
 
         //Glue
         glue.setFont(new Font("Arial", Font.PLAIN, 16));
         glue.setForeground(new Color(0x000000));
         panel.add(glue);
+        glue.setPreferredSize(new Dimension(100, 50));
         glue.setBounds(100, 150, 100, 50);
         panel.add(brandGlue2);
+        brandGlue2.setPreferredSize(new Dimension(100, 50));
         brandGlue2.setBounds(200, 150, 100, 50);
         panel.add(colorGlue1);
+        colorGlue1.setPreferredSize(new Dimension(100, 50));
         colorGlue1.setBounds(300, 150, 100, 50);
         panel.add(glueQuantity);
+        glueQuantity.setPreferredSize(new Dimension(100, 50));
         glueQuantity.setBounds(400, 150, 100, 50);
         panel.add(glueUnitPrice);
+        glueUnitPrice.setPreferredSize(new Dimension(100, 50));
         glueUnitPrice.setBounds(500, 150, 100, 50);
         panel.add(glueTotalAmount);
+        glueTotalAmount.setPreferredSize(new Dimension(100, 50));
         glueTotalAmount.setBounds(600, 150, 100, 50);
 
         //Notebook
         notebook.setFont(new Font("Arial", Font.PLAIN, 16));
         notebook.setForeground(new Color(0x000000));
         panel.add(notebook);
+        notebook.setPreferredSize(new Dimension(100, 50));
         notebook.setBounds(100, 200, 100, 50);
         panel.add(brandNotebook2);
         brandNotebook2.setBounds(200, 200, 100, 50);
@@ -196,86 +209,116 @@ public class ScreenOne extends JFrame {
         paper.setFont(new Font("Arial", Font.PLAIN, 16));
         paper.setForeground(new Color(0x000000));
         panel.add(paper);
+        paper.setPreferredSize(new Dimension(100, 50));
         paper.setBounds(100, 250, 100, 50);
         panel.add(brandPaper2);
+        brandPaper2.setPreferredSize(new Dimension(100, 50));
         brandPaper2.setBounds(200, 250, 100, 50);
         panel.add(colorPaper1);
+        colorPaper1.setPreferredSize(new Dimension(100, 50));
         colorPaper1.setBounds(300, 250, 100, 50);
         panel.add(paperQuantity);
+        paperQuantity.setPreferredSize(new Dimension(100, 50));
         paperQuantity.setBounds(400, 250, 100, 50);
         panel.add(paperUnitPrice);
+        paperUnitPrice.setPreferredSize(new Dimension(100, 50));
         paperUnitPrice.setBounds(500, 250, 100, 50);
         panel.add(paperTotalAmount);
+        paperTotalAmount.setPreferredSize(new Dimension(100, 50));
         paperTotalAmount.setBounds(600, 250, 100, 50);
 
         //Pen
         pen.setFont(new Font("Arial", Font.PLAIN, 16));
         pen.setForeground(new Color(0x000000));
         panel.add(pen);
+        pen.setPreferredSize(new Dimension(100, 50));
         pen.setBounds(100, 300, 100, 50);
         panel.add(brandPen2);
+        brandPen2.setPreferredSize(new Dimension(100, 50));
         brandPen2.setBounds(200, 300, 100, 50);
         panel.add(colorPen1);
+        colorPen1.setPreferredSize(new Dimension(100, 50));
         colorPen1.setBounds(300, 300, 100, 50);
         panel.add(penQuantity);
+        penQuantity.setPreferredSize(new Dimension(100, 50));
         penQuantity.setBounds(400, 300, 100, 50);
         panel.add(penUnitPrice);
+        penUnitPrice.setPreferredSize(new Dimension(100, 50));
         penUnitPrice.setBounds(500, 300, 100, 50);
         panel.add(penTotalAmount);
+        penTotalAmount.setPreferredSize(new Dimension(100, 50));
         penTotalAmount.setBounds(600, 300, 100, 50);
 
         //Pencil
         pencil.setFont(new Font("Arial", Font.PLAIN, 16));
         pencil.setForeground(new Color(0x000000));
         panel.add(pencil);
+        pencil.setPreferredSize(new Dimension(100, 50));
         pencil.setBounds(100, 350, 100, 50);
         panel.add(brandPencil2);
+        brandPencil2.setPreferredSize(new Dimension(100, 50));
         brandPencil2.setBounds(200, 350, 100, 50);
         panel.add(colorPencil1);
+        colorPencil1.setPreferredSize(new Dimension(100, 50));
         colorPencil1.setBounds(300, 350, 100, 50);
         panel.add(pencilQuantity);
+        pencilQuantity.setPreferredSize(new Dimension(100, 50));
         pencilQuantity.setBounds(400, 350, 100, 50);
         panel.add(pencilUnitPrice);
+        pencilUnitPrice.setPreferredSize(new Dimension(100, 50));
         pencilUnitPrice.setBounds(500, 350, 100, 50);
         panel.add(pencilTotalAmount);
+        pencilTotalAmount.setPreferredSize(new Dimension(100, 50));
         pencilTotalAmount.setBounds(600, 350, 100, 50);
 
         //Scissors
         scissors.setFont(new Font("Arial", Font.PLAIN, 16));
         scissors.setForeground(new Color(0x000000));
         panel.add(scissors);
+        scissors.setPreferredSize(new Dimension(100, 50));
         scissors.setBounds(100, 400, 100, 50);
         panel.add(brandScissors2);
+        brandScissors2.setPreferredSize(new Dimension(100, 50));
         brandScissors2.setBounds(200, 400, 100, 50);
         panel.add(colorScissors1);
+        colorScissors1.setPreferredSize(new Dimension(100, 50));
         colorScissors1.setBounds(300, 400, 100, 50);
         panel.add(scissorsQuantity);
+        scissorsQuantity.setPreferredSize(new Dimension(100, 50));
         scissorsQuantity.setBounds(400, 400, 100, 50);
         panel.add(scissorsUnitPrice);
+        scissorsUnitPrice.setPreferredSize(new Dimension(100, 50));
         scissorsUnitPrice.setBounds(500, 400, 100, 50);
         panel.add(scissorsTotalAmount);
+        scissorsTotalAmount.setPreferredSize(new Dimension(100, 50));
         scissorsTotalAmount.setBounds(600, 400, 100, 50);
 
         //Sharpener
         sharpener.setFont(new Font("Arial", Font.PLAIN, 16));
         sharpener.setForeground(new Color(0x000000));
         panel.add(sharpener);
+        sharpener.setPreferredSize(new Dimension(100, 50));
         sharpener.setBounds(100, 450, 100, 50);
         panel.add(brandSharpener2);
+        brandSharpener2.setPreferredSize(new Dimension(100, 50));
         brandSharpener2.setBounds(200, 450, 100, 50);
         panel.add(colorSharpener1);
+        colorSharpener1.setPreferredSize(new Dimension(100, 50));
         colorSharpener1.setBounds(300, 450, 100, 50);
         panel.add(sharpenerQuantity);
+        sharpenerQuantity.setPreferredSize(new Dimension(100, 50));
         sharpenerQuantity.setBounds(400, 450, 100, 50);
         panel.add(sharpenerUnitPrice);
+        sharpenerUnitPrice.setPreferredSize(new Dimension(100, 50));
         sharpenerUnitPrice.setBounds(500, 450, 100, 50);
         panel.add(sharpenerTotalAmount);
+        sharpenerTotalAmount.setPreferredSize(new Dimension(100, 50));
         sharpenerTotalAmount.setBounds(600, 450, 100, 50);
 
         //Button Save
         panel.add(save);
-        //save.setBounds(300, 520, 100, 50);
-        //save.setSize(100, 50);
+        save.setBounds(300, 520, 100, 50);
+        save.setPreferredSize(new Dimension(100, 50));
         //save.setLayout(null);
         //save.setVisible(true);
         save.setFont(new Font("Arial", Font.PLAIN, 16));
@@ -287,8 +330,8 @@ public class ScreenOne extends JFrame {
         //Button Delete
         panel.add(delete);
         //delete.setLayout(null);
-        //delete.setVisible(true);
-        //delete.setBounds(500, 520, 100, 50);
+        delete.setPreferredSize(new Dimension(100, 50));
+        delete.setBounds(500, 520, 100, 50);
         delete.setFont(new Font("Arial", Font.PLAIN, 16));
         delete.setForeground(new Color(0x000000));
         delete.setBackground(new Color(0xffffff));
