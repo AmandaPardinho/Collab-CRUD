9065bcabb1987ab5bf87a27bbf8bfed5a36da3e7
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-23 22:17:06 -0300

    Initiating table changes

--- screen/GenericItemScreen.java
+++ screen/GenericItemScreen.java
@@ -3,66 +3,90 @@ package screen;
 import javax.swing.*;
 import java.awt.*;
 
-public class GenericItemScreen {
+public class GenericItemScreen extends JPanel {
 
     //Frame
-    JFrame frame = new JFrame("Stock");
+    //JFrame frame = new JFrame("Stock");
 
     //Panel
-    JPanel panel = new JPanel();
+    //JPanel panelGeneric = new JPanel();
 
     //Generic Labels
-    JLabel product = new JLabel("Product");
-    JLabel brand = new JLabel("Brand");
-    JLabel color = new JLabel("Color");
-    JLabel quantity = new JLabel("Quantity");
-    JLabel unitPrice = new JLabel("Unit Price");
-    JLabel totalAmount= new JLabel("Total Amount");
+    private JLabel product = new JLabel("Product");
+    private JLabel brand = new JLabel("Brand");
+    private JLabel color = new JLabel("Color");
+    private JLabel quantity = new JLabel("Quantity");
+    private JLabel unitPrice = new JLabel("Unit Price");
+    private JLabel totalAmount= new JLabel("Total Amount");
 
-    public GenericItemScreen(){
-        //Generic Labels
-        panel.add(product);
-        product.setBounds(100, 20, 100, 50);
-        //product.setAlignmentX(100);
-        //product.setAlignmentY(20);
-        product.setPreferredSize(new Dimension(100, 50));
+    public GenericItemScreen() {
+        setLayout(new FlowLayout());
+
+        add(product);
+        //product.setBounds(100, 20, 100, 50);
+        product.setPreferredSize(new Dimension(100, 100));
         product.setFont(new Font("Arial", Font.BOLD, 16));
         product.setForeground(new Color(0x000000));
-        panel.add(brand);
-        //brand.setAlignmentX(200);
-        //brand.setAlignmentY(20);
+
+        add(brand);
         brand.setPreferredSize(new Dimension(100, 50));
         brand.setFont(new Font("Arial", Font.BOLD, 16));
         brand.setForeground(new Color(0x000000));
         brand.setBounds(200, 20, 100, 50);
-        panel.add(color);
-        //color.setAlignmentX(300);
-        //color.setAlignmentY(20);
+
+        add(color);
         color.setPreferredSize(new Dimension(100, 50));
         color.setFont(new Font("Arial", Font.BOLD, 16));
         color.setForeground(new Color(0x000000));
         color.setBounds(300, 20, 100, 50);
-        panel.add(quantity);
-        //quantity.setAlignmentX(400);
-        //quantity.setAlignmentY(20);
+
+        add(quantity);
         quantity.setPreferredSize(new Dimension(100, 50));
         quantity.setFont(new Font("Arial", Font.BOLD, 16));
         quantity.setForeground(new Color(0x000000));
         quantity.setBounds(400, 20, 100, 50);
-        panel.add(unitPrice);
-        //unitPrice.setAlignmentX(500);
-        //unitPrice.setAlignmentY(20);
+
+        add(unitPrice);
         unitPrice.setPreferredSize(new Dimension(100, 50));
         unitPrice.setFont(new Font("Arial", Font.BOLD, 16));
         unitPrice.setForeground(new Color(0x000000));
         unitPrice.setBounds(500, 20, 100, 50);
-        panel.add(totalAmount);
-        //totalAmount.setAlignmentX(600);
-        //totalAmount.setAlignmentY(20);
-        totalAmount.setPreferredSize(new Dimension(100, 50));
+
+        add(totalAmount);
+        totalAmount.setPreferredSize(new Dimension(100, 100));
         totalAmount.setFont(new Font("Arial", Font.BOLD, 16));
         totalAmount.setForeground(new Color(0x000000));
-        totalAmount.setBounds(600, 20, 100, 50);
+        //totalAmount.setBounds(600, 20, 100, 50);
+
+        setVisible(true);
+    }
+
+    /*public JPanel getPanelGeneric() {
+        return panelGeneric;
+    }*/
+
+    public JLabel getProduct() {
+        return product;
+    }
+
+    public JLabel getBrand() {
+        return brand;
+    }
+
+    public JLabel getColor() {
+        return color;
+    }
+
+    public JLabel getQuantity() {
+        return quantity;
+    }
+
+    public JLabel getUnitPrice() {
+        return unitPrice;
+    }
+
+    public JLabel getTotalAmount() {
+        return totalAmount;
     }
 
     public static void main(String[] args) {
--- screen/ScreenOne.java
+++ screen/ScreenOne.java
@@ -13,7 +13,7 @@ public class ScreenOne extends JFrame {
     JPanel panel = new JPanel();
 
     //Generic Array
-    String[] colorArray = {"", "None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
+    //String[] colorArray = {"", "None", "Black", "Blue", "Green", "Pink", "Purple", "Red", "White", "Yellow"};
 
     public ScreenOne() throws Exception{
 
@@ -28,10 +28,11 @@ public class ScreenOne extends JFrame {
         panel.setSize(800, 600);
         panel.setLocation(0,0);
         panel.setBackground(Color.cyan);
-        panel.setLayout(new GridLayout(10, 6));
+        panel.setLayout(new BorderLayout());
         panel.setVisible(true);
 
-
+        GenericItemScreen panelGIS = new GenericItemScreen();
+        panel.add(BorderLayout.NORTH, panelGIS);
     }
 
     public static void main(String[] args) throws Exception {
