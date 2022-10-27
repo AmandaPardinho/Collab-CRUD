793651856e60f350908f55d8cf46bff61a68c09a
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-27 16:21:16 -0300

    Inserting the components into the class QuantityAdd

--- addscreen/QuantityAdd.java
+++ addscreen/QuantityAdd.java
@@ -1,4 +1,28 @@
 package addscreen;
 
-public class QuantityAdd {
+import javax.swing.*;
+import java.awt.*;
+
+public class QuantityAdd extends JPanel {
+
+    //Label
+    private JLabel addQuantityLabel = new JLabel("Quantity");
+
+    //JTextField
+    private JTextField addQuantity = new JTextField(null, 8);
+
+    public QuantityAdd(){
+
+        add(addQuantityLabel);
+        addQuantityLabel.setBounds(400, 300, 100, 50);
+        addQuantityLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        addQuantityLabel.setForeground(new Color(0x000000));
+        add(addQuantity);
+        addQuantity.setBounds(550, 200, 100, 50);
+        addQuantity.setFont(new Font("Arial", Font.PLAIN, 16));
+        addQuantity.setForeground(new Color(0x000000));
+    }
+    public static void main(String[] args) {
+
+    }
 }
