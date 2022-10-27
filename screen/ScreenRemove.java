1b1643f309af799239c1d1ad0677c3980654d352
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-10-26 23:08:15 -0300

    Creating the JFrame, Jpanel and JMenuBar

--- screen/ScreenRemove.java
+++ screen/ScreenRemove.java
@@ -1,4 +1,18 @@
 package screen;
 
+import javax.swing.*;
+
 public class ScreenRemove {
+    //Frame
+    private JFrame frame = new JFrame("Remove Item");
+
+    //Panel
+    private JPanel panel1 = new JPanel();
+
+    //Menubar
+    private JMenuBar menuBar = new JMenuBar();
+
+    public static void main(String[] args) {
+
+    }
 }
